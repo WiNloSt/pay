d16f33a2f016cfe888b4a8a9b91c34bec2a610a9
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-07-18 19:24:52 +0700

    :wrench: Exclude no-example component from styleguidist

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   components: 'src/components/**/*.js',
+  skipComponentsWithoutExample: true,
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/styleguide/Wrapper')
   },
