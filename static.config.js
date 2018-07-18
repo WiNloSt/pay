34e00dbc3643406ff773a26d205b8db688eaf079
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-07-18 22:09:45 +0700

    :fire: Remove unused pages

--- static.config.js
+++ static.config.js
@@ -1,39 +1,16 @@
-import axios from 'axios'
 import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
 export default {
-  getSiteData: () => ({
-    title: 'React Static'
-  }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
     return [
       {
         path: '/',
-        component: 'src/containers/Home'
-      },
-      {
-        path: '/about',
-        component: 'src/containers/About'
-      },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post
-          })
-        }))
+        component: 'src/pages/Home'
       },
       {
         is404: true,
-        component: 'src/containers/404'
+        component: 'src/pages/404'
       }
     ]
   },
